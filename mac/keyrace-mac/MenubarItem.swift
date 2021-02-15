9a4cc347893ca0ce6bbe84692716595e3e1c6b42
Nat Friedman <nat@nat.org>
2021-02-14 18:14:38 -0800

    Initial commit of native mac client

---
+++ mac/keyrace-mac/MenubarItem.swift
@@ -0,0 +1,196 @@
+//
+//  MenubarItem.swift
+//  keyrace-mac
+//
+//  Created by Nat Friedman on 1/3/21.
+//
+
+import Foundation
+import Cocoa
+import SwiftUI
+import Charts
+
+class ChartValueFormatter: NSObject, IValueFormatter {
+
+    func stringForValue(_ value: Double, entry: ChartDataEntry, dataSetIndex: Int, viewPortHandler: ViewPortHandler?) -> String {
+        if value == 0.0 {
+            return ""
+        }
+
+        return String(Int(value))
+    }
+}
+
+class TypingChart: BarChartView {
+    
+    func NewData(_ typingCount: [Int]) {
+        
+        let yse1 = typingCount.enumerated().map { x, y in return BarChartDataEntry(x: Double(x), y: Double(y)) }
+
+        let data = BarChartData()
+        let ds1 = BarChartDataSet(entries: yse1, label: "Hello")
+        ds1.colors = [NSUIColor.init(srgbRed: 255.0/255.0, green: 255.0/255.0, blue: 0.0/255.0, alpha: 1.0)]
+        data.addDataSet(ds1)
+        data.barWidth = Double(0.5)
+        data.setDrawValues(true)
+        let valueFormatter = ChartValueFormatter()
+        data.setValueFormatter(valueFormatter)
+        
+        self.data = data
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let xArray = Array(1..<24)
+        let ys1 = xArray.map { x in return abs(sin(Double(x) / 2.0 / 3.141 * 1.5)) }
+        
+        let yse1 = ys1.enumerated().map { x, y in return BarChartDataEntry(x: Double(x), y: y) }
+
+        let data = BarChartData()
+        let ds1 = BarChartDataSet(entries: yse1, label: "Hello")
+        ds1.colors = [NSUIColor.red]
+        data.addDataSet(ds1)
+        data.barWidth = Double(0.5)
+        
+        self.data = data
+        
+        self.legend.enabled = false
+        self.leftAxis.drawGridLinesEnabled = false
+        self.leftAxis.drawAxisLineEnabled = false
+        self.leftAxis.drawLabelsEnabled = false
+        self.rightAxis.drawGridLinesEnabled = false
+        self.rightAxis.drawAxisLineEnabled = false
+        self.rightAxis.drawLabelsEnabled = false
+        self.xAxis.drawGridLinesEnabled = false
+        self.xAxis.drawAxisLineEnabled = false
+        self.xAxis.drawLabelsEnabled = false
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class MenubarItem : NSObject {
+    private var loginMenuItem : NSMenuItem
+    private var quitMenuItem : NSMenuItem
+    private var barChartItem : NSMenuItem
+    private var leaderboardItem : NSMenuItem
+    
+    var gh : GitHub? {
+        didSet {
+            if gh!.loggedIn {
+                loggedIn()
+            }
+        }
+    }
+    
+    var keyTap : KeyTap?
+
+    var statusBarItem : NSStatusItem = {
+        let item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        return item
+    }()
+
+    let statusBarMenu = NSMenu(title: "foo")
+
+    init(title: String) {
+        loginMenuItem = NSMenuItem(title: "Login with GitHub", action: #selector(login), keyEquivalent: "")
+        quitMenuItem = NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "")
+        barChartItem = NSMenuItem()
+        leaderboardItem = NSMenuItem()
+
+        super.init()
+
+        statusBarItem.button?.title = title
+
+        let barChart = TypingChart(frame: CGRect(x: 0, y: 0, width: 350, height: 100))
+        barChartItem.view = barChart
+
+        let leaderboard = NSTextView(frame: CGRect(x: 0, y: 0, width: 350, height: 200))
+        leaderboard.string = ""
+        leaderboard.font = NSFont(name:"Helvetica Bold", size:12)
+        leaderboardItem.view = leaderboard
+        
+        quitMenuItem.target = self
+        loginMenuItem.target = self
+
+        
+        statusBarMenu.addItem(barChartItem)
+        statusBarMenu.addItem(leaderboardItem)
+        statusBarMenu.addItem(loginMenuItem)
+        statusBarMenu.addItem(quitMenuItem)
+        statusBarItem.menu = statusBarMenu
+        
+        statusBarMenu.delegate = self
+    }
+    
+    func loggedIn() {
+        if gh?.username != nil {
+            loginMenuItem.title = "Logged in as @" + gh!.username!
+            loginMenuItem.isEnabled = false
+            loginMenuItem.target = nil
+        }
+    }
+    
+    @objc func login() {
+        if gh == nil {
+            return
+        }
+        
+        if gh!.token != nil {
+            gh!.getUserName()
+            loggedIn()
+            return
+        }
+
+        let alert = NSAlert()
+        alert.messageText = "Login with GitHub"
+
+        DispatchQueue.global(qos: .background).async {
+            let (userCode, verificationUri) = self.gh!.startDeviceAuth(clientId: "a945f87ad537bfddb109", scope: "", callback: self.loggedIn)
+            
+            if (userCode == "") {
+                alert.informativeText = "Could not contact GitHub. Try again later."
+                alert.addButton(withTitle: "Ok")
+                alert.runModal()
+                return
+            }
+            
+            DispatchQueue.main.async {
+                alert.informativeText = "Your GitHub device code is \(userCode)"
+                alert.addButton(withTitle: "Copy device code and open browser")
+                alert.addButton(withTitle: "Cancel")
+
+                let modalResult = alert.runModal()
+                switch modalResult {
+                case .alertFirstButtonReturn:
+                    let pasteboard = NSPasteboard.general
+                    pasteboard.clearContents()
+                    pasteboard.setString(userCode, forType: .string)
+                    let url = URL(string: verificationUri)!
+                    NSWorkspace.shared.open(url)
+                default:
+                    return
+                }
+            }
+        }
+    }
+
+    @objc func quit() {
+        print("quitting")
+        exit(0)
+    }
+}
+
+extension MenubarItem : NSMenuDelegate {
+    func menuWillOpen(_ menu: NSMenu) {
+        print("Menu will open!")
+        
+        // Update the bar chart
+        (self.barChartItem.view as? TypingChart)?.NewData((keyTap?.getChart())!)
+        (self.leaderboardItem.view as? NSTextView)?.string = (keyTap?.getLeaderboardText())!
+    }
+}
---
+++ mac/keyrace-mac/keyrace_macApp.swift
@@ -0,0 +1,296 @@
+//
+//  keyrace-mac
+//
+//  Created by Nat Friedman on 1/2/21.
+//
+
+// TODO:
+// - Restructure, cleanup, learn swift
+// - Save username locally along with token
+// - Talk to server and show leaderboard
+
+
+import SwiftUI
+import Foundation
+import Accessibility
+import Cocoa
+
+@available(OSX 11.0, *)
+@main
+struct MenuBarPopoverApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var body: some Scene {
+        Settings{
+            EmptyView()
+        }
+    }
+}
+
+func formatCount(count: Int) -> String {
+    var str = ""
+
+    if (count == 0) {
+        str = "Waiting for first keystroke..."
+    } else if (count == 1) {
+        str = "👍 First key!"
+    } else {
+        var pfx = ""
+        switch (count) {
+        case 1..<500:       pfx = "👍 "
+        case 500..<1000:    pfx = "🏃 "
+        case 1000..<5000:   pfx = "💨 "
+        case 5000..<10000:  pfx = "🙌 "
+        case 10000..<20000: pfx = "🚀 "
+        case 20000..<30000: pfx = "🥳 "
+        case 30000...40000: pfx = "🔥 "
+        case 40000...60000: pfx = "🤯 "
+        default:
+            pfx = ""
+        }
+        
+        var sfx = ""
+        if (count < 100) {
+            sfx = " today"
+        }
+        str = "\(pfx)\(count) keys\(sfx)"
+    }
+
+    return str
+}
+
+func myCGEventCallback(proxy: CGEventTapProxy, type: CGEventType, event: CGEvent, refcon: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? {
+    let keyTap = Unmanaged<KeyTap>.fromOpaque(refcon!).takeUnretainedValue()
+
+    if [.keyDown].contains(type) {
+
+        keyTap.increment()
+        keyTap.appDelegate.menubarItem?.statusBarItem.button?.title = formatCount(count: keyTap.keycount)
+    }
+
+    if [.tapDisabledByTimeout].contains(type) {
+        keyTap.appDelegate.menubarItem?.statusBarItem.button?.title = "Lost event tap!"
+    }
+
+    return Unmanaged.passRetained(event)
+}
+
+class KeyTap {
+    var appDelegate : AppDelegate
+    var keycount = 0
+    var lastDay = -1
+    var lastMin = -1
+    var timerRunning = false
+    var keyTrapSetup = false
+    var KEYRACE_HOST = "keyrace.app"
+    var minutes = [Int](repeating:0, count:1440)
+    var leaderboardText = "Loading leaderboard..."
+    
+    init(_ appd: AppDelegate) {
+        self.appDelegate = appd
+    }
+    
+    func increment() {
+        let date = Date()
+        let calendar = Calendar.current
+
+        // Reset to 0 at midnight
+        let day = calendar.component(.day, from:date)
+        if (lastDay != day) {
+            lastDay = day
+            keycount = 0
+        }
+        
+        keycount += 1
+        
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        minutes[hour*60 + minute] += 1
+
+        // Upload every minute
+        if (lastMin != minute) {
+            lastMin = minute
+            uploadCount()
+        }
+        
+        saveCount()
+    }
+    
+    func getChart() -> [Int] {
+        // Return the last 20 minutes minutely
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let min = calendar.component(.minute, from: date)
+        let currMin = hour*60 + min
+        return Array(minutes[currMin - 20...currMin])
+    }
+
+    func getLeaderboardText() -> String {
+        return leaderboardText
+    }
+    
+    func uploadKeycount() {
+        if appDelegate.gh?.token == nil {
+            return
+        }
+
+        var url = URLComponents(string: "https://\(KEYRACE_HOST)/count")!
+        url.queryItems = [
+            URLQueryItem(name: "count", value: "\(keycount)")
+        ]
+        var request = URLRequest(url: url.url!)
+        request.addValue("Bearer \(appDelegate.gh!.token!)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data,                            // is there data
+                let response = response as? HTTPURLResponse,  // is there HTTP response
+                (200 ..< 300) ~= response.statusCode,         // is statusCode 2XX
+                error == nil else {                           // was there no error, otherwise ...
+                    print("Error uploading count \(error!)")
+                    return
+            }
+
+            let responseObject = (try? JSONSerialization.jsonObject(with: data)) as? [String: Any]
+            // FIXME
+            self.leaderboardText = "Fill this in with the leaderboard text"
+        }
+        task.resume()
+    }
+    
+    func setupKeyTap() {
+        if (keyTrapSetup) {
+            return
+        }
+        
+        loadCount()
+
+        let eventMask = (1 << CGEventType.keyDown.rawValue)
+        let refcon = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+        guard let eventTap = CGEvent.tapCreate(tap: .cgSessionEventTap,
+                                              place: .headInsertEventTap,
+                                              options: .defaultTap,
+                                              eventsOfInterest: CGEventMask(eventMask),
+                                              callback: myCGEventCallback,
+                                              userInfo: refcon) else {
+            NSLog("failed to create event tap; quitting")
+            exit(1)
+        }
+        keyTrapSetup = true
+
+        let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+        CGEvent.tapEnable(tap: eventTap, enable: true)
+
+        appDelegate.menubarItem!.statusBarItem.button?.title = formatCount(count: keycount)
+    }
+    
+    func uploadCount () {
+        print("Uploading count \(keycount) now...")
+        uploadKeycount()
+    }
+    
+    func saveCount() {
+        var filename = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".keyrace.tmp")
+
+        var str = String(keycount)
+        do {
+            try str.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            NSLog("Could not write keycount to \(filename.path)")
+            // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+        }
+        
+        filename = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".keyrace.minutes.tmp")
+        let minStrings = minutes.map({ String($0) })
+        str = minStrings.joined(separator: ",")
+        do {
+            try str.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            NSLog("Could not write keycount to \(filename.path)")
+            // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+        }
+
+    }
+    
+    func loadCount() {
+        let date = Date()
+        let calendar = Calendar.current
+        let day = calendar.component(.day, from: date)
+        let month = calendar.component(.month, from:date)
+
+        // Load the total daily count
+        var filename = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".keyrace.tmp")
+        do {
+            let attr = try FileManager.default.attributesOfItem(atPath: filename.path)
+            let mtime = attr[FileAttributeKey.modificationDate] as! Date
+            lastDay = calendar.component(.day, from:mtime)
+            let lastMonth = calendar.component(.month, from:mtime)
+            
+            if (lastDay == day && lastMonth == month) {
+                let str = try String(contentsOf: filename, encoding: .utf8)
+                keycount = Int(str) ?? 0
+            }
+        } catch { }
+        
+        // Load the hourly histogram
+        filename = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".keyrace.minutes.tmp")
+        do {
+            let attr = try FileManager.default.attributesOfItem(atPath: filename.path)
+            let mtime = attr[FileAttributeKey.modificationDate] as! Date
+            let lastDay = calendar.component(.day, from:mtime)
+            let lastMonth = calendar.component(.month, from:mtime)
+
+            if (lastDay == day && lastMonth == month) {
+                let str = try String(contentsOf: filename, encoding: .utf8)
+                let minStr = str.split(separator: ",")
+                minutes = minStr.map { x in return Int(x) ?? 0 }
+            }
+        } catch { }
+
+
+    }
+
+    func getAccessibilityPermissions() {
+        let options = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString: true]
+        var isAppTrusted = AXIsProcessTrustedWithOptions(options as CFDictionary?)
+        if (isAppTrusted) {
+            self.setupKeyTap()
+            return
+        }
+        
+        // Wait for the user to give us permission
+        if (timerRunning) { return }
+        self.timerRunning = true
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            let options = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString: false]
+            isAppTrusted = AXIsProcessTrustedWithOptions(options as CFDictionary?)
+            if (isAppTrusted) {
+                self.setupKeyTap()
+                timer.invalidate()
+                self.timerRunning = false
+            }
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusBarItem: NSStatusItem!
+    var menubarItem: MenubarItem?
+    var keyTap : KeyTap?
+    var gh : GitHub?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        menubarItem = MenubarItem(title: "Setup Keyrace")
+
+        gh = GitHub()
+        menubarItem?.gh = gh
+
+        keyTap = KeyTap(self)
+        menubarItem?.keyTap = keyTap
+        keyTap!.getAccessibilityPermissions()
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
