9af94953d66c0c964b218f8c0b7e9923a8b6ffed
Thomas Dohmke <thomas@dohmke.de>
2021-02-19 13:43:56 -0800

    align file names and class per file structure with Swift guidelines

---
+++ mac/keyrace-mac/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  keyrace-mac
+//
+//  Created by Nat Friedman on 1/2/21.
+//
+
+import Foundation
+import SwiftUI
+
+@available(OSX 11.0, *)
+@main
+struct MenuBarPopoverApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var body: some Scene {
+        Settings{
+            EmptyView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusBarItem: NSStatusItem!
+    var menubarItem: MenubarItem?
+    var keyTap : KeyTap?
+    var gh : GitHub?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        menubarItem = MenubarItem(title: "Setup Keyrace")
+
+        gh = GitHub()
+        menubarItem?.gh = gh
+
+        keyTap = KeyTap(self)
+        menubarItem?.keyTap = keyTap
+        keyTap!.getAccessibilityPermissions()
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
+
---
+++ mac/keyrace-mac/NSImage+circle.swift
@@ -0,0 +1,20 @@
+// Created by Thomas Dohmke on 2/19/21.
+
+import Foundation
+import SwiftUI
+
+extension NSImage {
+    // Copies this image to a new one with a circular mask.
+    func circle() -> NSImage {
+        let image = NSImage(size: size)
+        image.lockFocus()
+
+        NSGraphicsContext.current?.imageInterpolation = .high
+        let frame = NSRect(origin: .zero, size: size)
+        NSBezierPath(ovalIn: frame).addClip()
+        draw(at: .zero, from: frame, operation: .sourceOver, fraction: 1)
+
+        image.unlockFocus()
+        return image
+    }
+}
