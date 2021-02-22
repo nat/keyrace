dca0dc7820002442338086dccb2930d8bcf52d22
Jess Frazelle <jess@oxide.computer>
2021-02-22 10:40:37 -0800

    keyboard heatmap with magic keyboard
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- mac/keyrace-mac/KeyboardView.swift
+++ mac/keyrace-mac/KeyboardView.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 let ROWS: [[[String]]] = [
+    [["", ""]],
     [["~", "`"],
     ["!", "1"],
     ["@", "2"],
@@ -22,79 +23,97 @@ let ROWS: [[[String]]] = [
     [")", "0"],
     ["_", "-"],
     ["+", "="]],
-    [["q", ""],
-    ["w", ""],
-    ["e", ""],
-    ["r", ""],
-    ["t", ""],
-    ["y", ""],
-    ["u", ""],
-    ["i", ""],
-    ["o", ""],
-    ["p", ""],
+    [["Q", "q"],
+    ["W", "w"],
+    ["E", "e"],
+    ["R", "r"],
+    ["T", "t"],
+    ["Y", "y"],
+    ["U", "u"],
+    ["I", "i"],
+    ["O", "o"],
+    ["P", "p"],
     ["{", "["],
     ["}", "]"],
     ["|", "\\"]],
-    [["a", ""],
-    ["s", ""],
-    ["d", ""],
-    ["f", ""],
-    ["g", ""],
-    ["h", ""],
-    ["j", ""],
-    ["k", ""],
-    ["l", ""],
+    [["A", "a"],
+    ["S", "s"],
+    ["D", "d"],
+    ["F", "f"],
+    ["G", "g"],
+    ["H", "h"],
+    ["J", "j"],
+    ["K", "k"],
+    ["L", "l"],
     [":", ";"],
     ["\"", "'"]],
-    [["z", ""],
-    ["x", ""],
-    ["c", ""],
-    ["v", ""],
-    ["b", ""],
-    ["n", ""],
-    ["m", ""],
+    [["Z", "z"],
+    ["X", "x"],
+    ["C", "c"],
+    ["V", "v"],
+    ["B", "b"],
+    ["N", "n"],
+    ["M", "m"],
     ["<", ","],
     [">", "."],
-    ["?", "/"]]]
+    ["?", "/"]],
+    [["", ""]]]
 
 struct KeyboardView: View {
     @ObservedObject var keyTap: KeyTap
     
     var body: some View {
-        ForEach(
-            ROWS,
-            id: \.self
-        ) { row in
-            Section {
-                HStack(alignment: .top, spacing: 4) {
-                    ForEach(
-                        row,
-                        id: \.self
-                    ) { char in
-                        KeyboardKey(keyTap: keyTap, char: char)
+        VStack(alignment: .center, spacing: 0) {
+            ForEach(
+                ROWS.indexed(),
+                id: \.1.self
+            ) { index, row in
+                Section {
+                    HStack(alignment: .center, spacing: 0) {
+                        ForEach(
+                            row,
+                            id: \.self
+                        ) { char in
+                            KeyView(keyTap: keyTap, char: char, index: index)
+                                .padding(.top, 1.4)
+                                .padding(.leading, -3.1)
+                                .padding(.trailing, -3.1)
+                                .padding(.bottom, 1.4)
+                        }
                     }
+                    .frame(width: 344.4, height: 20.7, alignment: index.getRowAlignment())
+                    .padding(.top, 1.4)
+                    .padding(.bottom, 1.4)
+                    .padding(.leading, 0)
+                    .padding(.trailing, 0)
                 }
-                .frame(width: 350, height: 26, alignment: .center)
             }
         }
+        .background(
+            Image("magic-keyboard")
+                .resizable()
+                .frame(width: 350, height: 144)
+        )
+        .padding(.leading, 8)
+        .padding(.trailing, 8)
     }
 }
 
-struct KeyboardKey: View {
+struct KeyView: View {
     var keyTap: KeyTap
     var char: [String]
+    var index: Int
     
     var body: some View {
-        Text((char[0] + " " + char[1]).trimmingCharacters(in: .whitespacesAndNewlines))
-            .background(Rectangle()
-                            .fill(Color.purple.opacity(
-                                char.getOpacity(keyTap)
-                            ))
-                            .frame(width: 22, height: 22)
-                            .foregroundColor(.white))
-            .font(.system(size: 8, weight: .medium, design: .monospaced))
-            .frame(width: 22, height: 22)
-            .help(String(format: "%d", keyTap.keyboardData[char]!))
+        if index == 0 || index == 5 {
+            Rectangle()
+                .frame(width: 20.7, height: 20.7)
+                .hidden()
+        } else {
+            Circle()
+                .fill(char.getFill(keyTap))
+                .frame(width: 30, height: 30)
+        }
     }
 }
 
@@ -102,6 +121,17 @@ extension String {
     func toUnicodeScalarInt() -> Int {
         let scalar = Unicode.Scalar.init(self as String) ?? Unicode.Scalar.init(0 as UInt8)
         return Int(scalar.value)
+    }
+}
+
+extension Int {
+    func getRowAlignment() -> Alignment {
+        if self == 1 {
+            return .leading
+        } else if self == 2 {
+            return .trailing
+        }
+        return .center
     }
 }
 
@@ -121,8 +151,24 @@ extension Array where Element == String  {
         return count
     }
     
-    func getOpacity(_ keyTap: KeyTap) -> Double {
-        let opacity = Double(keyTap.keyboardData[self]!) / Double(keyTap.maxKeyboardCount)
-        return opacity
+    func getFill(_ keyTap: KeyTap) -> RadialGradient {
+        let percentage = (Double(keyTap.keyboardData[self]!) / Double(keyTap.maxKeyboardCount)) * 100
+        
+        // Calculate the color and the percentage.
+        var color = Color.green
+        var opacity = percentage / 33
+        if percentage > 33 && percentage < 66 {
+            color = Color.yellow
+            opacity = percentage / 66
+        } else if percentage >= 66 {
+            color = Color.red
+            opacity = percentage / 100
+        }
+        
+        return RadialGradient(
+            gradient: Gradient(colors: [color.opacity(opacity), color.opacity(opacity / 1.6), color.opacity(0.1)]),
+            center: .center,
+            startRadius: 0,
+            endRadius: 15)
     }
 }
